62b6c44ed47f6cd294d15b3030a42aba1bdd1947
Guillaume Archambault <archambault.guillaume06@gmail.com>
2023-04-28 17:03:47 +0200

    Changes to be committed: 	new file:   Powerlevel10k/p10k_nographic.zsh 	modified:   macos.sh 	modified:   ubuntu.sh

--- macos.sh
+++ macos.sh
@@ -27,35 +27,37 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-sy
 # Alias #
 
 # Lignes à ajouter à .zshrc
-aliases=(
-    '#neofetch'
-    'neofetch --config ~/.config/neofetch/.neofetch.conf
-    'bash ~/scripts/kaamelott.sh'
-    'echo ""'
-    ''
-    '### Alias'
-    'alias cat='\'bat -p\'''
-    'alias catn=bat'
-    ''
-    'alias ls='\'exa --grid --color auto --icons --sort=type\'''
-    'alias ll='\'exa --long --color always --icons --sort=type\'''
-    'alias la='\'exa --grid --all --color auto --icons --sort=type\'''
-    'alias lla='\'exa --long --all --color auto --icons --sort=type\'''
-    ''
-    '### zsh-autocompletion'
-    'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh'
-    ''
-    '### zsh-syntax-highlighting'
-    'source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
-    ''
-    '### thefuck'
-    'eval $(thefuck --alias)'
-)
+echo "
+#neofetch
+neofetch --config ~/.config/neofetch/.neofetch.conf
+echo ''
+### Alias
+alias cat='bat -p'
+alias catn=bat
 
-# Ajouter les lignes à .zshrc
-for line in "${aliases[@]}"; do
-    echo "$aliases" >> ~/.zshrc
-done
+alias ls='exa --grid --color auto --icons --sort=type'
+alias ll='exa --long --color always --icons --sort=type'
+alias la='exa --grid --all --color auto --icons --sort=type'
+alias lla='exa --long --all --color auto --icons --sort=type'
+
+### zsh-autocompletion
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+### zsh-syntax-highlighting
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+### thefuck
+eval $(thefuck --alias)
+" >> /home/${USER}/.zshrc
+
+# Install pk10 and fonts
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 # Files and folders copy
+if [ ! -d ~/.config/neofetch ]; then
+  mkdir -p ~/.config/neofetch
+fi
 cp -r ./neofetch/* ~/.config/neofetch
--- ubuntu.sh
+++ ubuntu.sh
@@ -2,13 +2,13 @@
 
 # Packages list
 packages=(
+    "zsh"     # Because zsh ...
     "micro"   # Better than nano
     "exa"     # Better than ls
     "bat"     # Better than cat
     "btop"    # Betther than top
     "ncdu"    # Better than du -h
     "duf"     # Better than df -h
-    "fuck"    # Fuck error
 )
 
 # Packages installation
@@ -16,6 +16,15 @@ sudo apt update
 for package in "${packages[@]}"; do
     sudo apt install "$package"
 done
+
+# zsh by default
+chsh -s $(which zsh)
+
+# Fuck
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
+echo ### thefuck
+eval $(thefuck --alias)
 
 # Git installation #
 
@@ -31,17 +40,16 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-sy
 echo "
 #neofetch
 neofetch --config ~/.config/neofetch/.neofetch.conf
-bash ~/scripts/kaamelott.sh
 echo ''
 
 ### Alias
-alias cat='\'bat -p\''
-alias catn=bat
+alias cat='batcat -p'
+alias catn=batcat
 
-alias ls='\'exa --grid --color auto --icons --sort=type\''
-alias ll='\'exa --long --color always --icons --sort=type\''
-alias la='\'exa --grid --all --color auto --icons --sort=type\''
-alias lla='\'exa --long --all --color auto --icons --sort=type\''
+alias ls='exa --grid --color auto --icons --sort=type'
+alias ll='exa --long --color always --icons --sort=type'
+alias la='exa --grid --all --color auto --icons --sort=type'
+alias lla='exa --long --all --color auto --icons --sort=type'
 
 ### zsh-autocompletion
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
@@ -49,8 +57,6 @@ source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 ### zsh-syntax-highlighting
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-### thefuck
-eval $(thefuck --alias)
 " >> /home/${USER}/.zshrc
 
 # Files and folders copy
@@ -58,3 +64,5 @@ if [ ! -d ~/.config/neofetch ]; then
   mkdir -p ~/.config/neofetch
 fi
 cp -r ./neofetch/* ~/.config/neofetch
+
+cp ./Powerlevel10k/p10k_nographic.zsh ~/.p10k.zsh
